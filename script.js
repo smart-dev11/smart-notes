411cea623dfd644bb7287de56bc473e6a751eab5
smart-dev11 <pc1162011@gmail.com>
2026-02-03 22:14:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+let notes = JSON.parse(localStorage.getItem("notes")) || [];
+let editIndex = null;
+
+const list = document.getElementById("notesList");
+
+function saveAndRender() {
+  localStorage.setItem("notes", JSON.stringify(notes));
+  render();
+}
+
+function addNote() {
+  const input = document.getElementById("noteInput");
+  if (input.value.trim() === "") return;
+
+  notes.unshift({
+    text: input.value,
+    priority: "low",
+    time: Date.now()
+  });
+
+  input.value = "";
+  saveAndRender();
+}
+
+function deleteNote(index) {
+  notes.splice(index, 1);
+  saveAndRender();
+}
+
+function openEdit(index) {
+  editIndex = index;
+  document.getElementById("editInput").value = notes[index].text;
+  document.getElementById("prioritySelect").value = notes[index].priority;
+  document.getElementById("modal").classList.remove("hidden");
+}
+
+function closeModal() {
+  document.getElementById("modal").classList.add("hidden");
+}
+
+function saveEdit() {
+  notes[editIndex].text = document.getElementById("editInput").value;
+  notes[editIndex].priority = document.getElementById("prioritySelect").value;
+  saveAndRender();
+  closeModal();
+}
+
+function sortNotes() {
+  const sortType = document.getElementById("sortSelect").value;
+
+  if (sortType === "recent") {
+    notes.sort((a, b) => b.time - a.time);
+  } else {
+    const order = { high: 3, medium: 2, low: 1 };
+    notes.sort((a, b) => order[b.priority] - order[a.priority]);
+  }
+
+  render();
+}
+
+function render() {
+  list.innerHTML = "";
+
+  notes.forEach((note, index) => {
+    const li = document.createElement("li");
+    li.classList.add(`note-${note.priority}`);
+
+    li.innerHTML = `
+      <span onclick="openEdit(${index})">${note.text}</span>
+      <button onclick="deleteNote(${index})">X</button>
+    `;
+
+    list.appendChild(li);
+  });
+}
+
+render();
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("sw.js");
+}
